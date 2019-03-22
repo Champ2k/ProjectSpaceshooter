27acecffd13179dc8c62da7eea0f680ba8791d16
Zexal2k <thehitmanza1@gmail.com>
2019-03-22 14:44:50 +0700

    windowscreen

--- Spaceshooter.py
+++ Spaceshooter.py
@@ -1,1 +1,39 @@
 import arcade
+WIDTH = 500
+HEIGHT = 750
+
+class SpaceWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+        self.world = arcade.load_texture('background.png')
+    
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_texture_rectangle(WIDTH // 2, HEIGHT // 2,
+                                      WIDTH, HEIGHT, self.world)
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+
+def main():
+    SpaceWindow()
+    arcade.run()
+
+if __name__ == '__main__':
+    window = SpaceWindow(WIDTH, HEIGHT)
+    arcade.run()
